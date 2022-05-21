70d97d5aa23a06e7486530803759211d186234d9
Daniel Hernandez-Otero <danielhotero96@gmail.com>
2022-05-21 00:40:45 -0400

    Slight re-organization, making sure dictionary still works

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 import discord
 
-#client = discord.Client()
-
 # Initiate intents for the bot
 # These lines grant the bot permissions, such as:
 # Viewing the list of members
@@ -12,23 +10,19 @@ intents.presences = True
 
 # Declare global variables, dicts and what have you.
 channel = discord.TextChannel
+method_dic = {"register": register_member, "unregister": unregister_member}
 
 # Set this to true if you feel like debugging anything
 debugging = True
 
 # END declaring global variables
 
+
 # Bot Token stored in Environment Variables
-# TODO: Fix this, actually add Discord Bot Token to environment variables
 bot_token = os.environ['TOKEN']
 
 # Initialize client. Used for all sorts of everything
 client = discord.Client(intents=intents)
-
-# TODO: Initiate variable that keeps track of probability
-# of the bot using @everyone. Variable should be able to
-# persist between Bot lifecycles as the bot crashes and
-# boots itself back up.
 
 
 # Event that activates as soon as the bot wakes up
@@ -47,17 +41,7 @@ async def on_ready():
 						'~uwu~ :: It looks like that channel wannel doesn\'t existy wisty! :: ~owo~'
 				)
 
-				danny_pikmin = client.get_user(102610839558688768)
 
-				try:
-						print(
-								f'Successfully found {danny_pikmin}. Use the debug console to find out more information.\n{danny_pikmin.activity.name} {danny_pikmin.activity.large_image_text} {danny_pikmin.activity.small_image_text}\n{danny_pikmin.activity.status} {danny_pikmin.activity.type} {danny_pikmin.activity.details}'
-						)
-				except AttributeError:
-						print("No activity is detected.")
-						pass
-
-					
 # Method to register a member to the book club
 async def register_member():
 	# TODO: Register member
@@ -78,9 +62,6 @@ async def unregister_member():
 		print('AttributeErr')
 
 
-method_dic = {"register": register_member, "unregister": unregister_member}
-
-
 @client.event
 async def on_message(message):
 	if message.author == client.user:
@@ -95,7 +76,7 @@ async def on_message(message):
 
 		if split[1] in method_dic:
 			await method_dic[split[1]]()
-			print(f'The command is {split[1]}')
+			print(f'The command is {split[1]}, calling method {method_dic[split[1]]}')
 		else:
 			print(f'The command {split[1]} does not coincide with any method')
 
